a84e78fc6944a26801f9af20c15e40bc1189530f
AndriuxDev <86565812+AndriuxDev@users.noreply.github.com>
2024-09-22 07:41:39 +0000

    Added credits.

--- data/final-round/script.lua
+++ data/final-round/script.lua
@@ -11,13 +11,31 @@ end
 
 function onUpdate()
     --OG Mickey was using a lot of space, will use a change icon event instead in V4.
-    
-    if curBeat == 543 then
-        makeLuaSprite('blackTransition', nil, 0, 0);
-	    makeGraphic('blackTransition', screenWidth * 2, screenHeight * 2, '000000')
-        updateHitbox('blackTransition');
-        setGraphicSize('blackTransition',1280,720);
-        setObjectCamera('blackTransition','camHud');
-	    addLuaSprite('blackTransition', false);
+    if curBeat == 64 then
+        setProperty("blackTransition.alpha", 0)
+    end
+
+    if curBeat == 248 then
+        runTimer('blackZoom', 0.1);
+    end
+
+    if curBeat == 260 then
+        setProperty("blackTransition.alpha", 0)
+    end
+
+    if curBeat == 544 then
+        setProperty("blackTransition.alpha", 1)
+    end
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'blackZoom' then
+		doTweenAlpha('blackTransitionTwn', 'blackTransition', 1, 1, 'linear');
+	end
+end
+
+function onTweenCompleted(tag)
+	if tag == 'blackTransitionTwn' then
+        setProperty("blackTransition.alpha", 1);
     end
 end
